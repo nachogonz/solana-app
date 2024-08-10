a36cf20917c5c4fa7e9ce3ff7f21cc1e61c2a585
nakk <nachogonzalez.dev@gmail.com>
2024-08-11 01:40:57 +0200

    feat: changes

--- src/components/AirdropContent.tsx
+++ src/components/AirdropContent.tsx
@@ -5,7 +5,7 @@ import useSolanaConnection from "@/hooks/useSolanaConnection";
 
 const AirdropContent = () => {
   const { publicKey } = useSolanaConnection();
-  return <div>{publicKey ? <AirdropButton /> : <h1 className="text-4xl">Connect Wallet To Start Airdrop</h1>}</div>;
+  return <div>{publicKey ? <AirdropButton /> : <h1 className="text-4xl">Connect Wallet To Start App</h1>}</div>;
 };
 
 export default AirdropContent;
